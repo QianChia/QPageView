5ff413bbd3a1c3e10e45e352352499d5caba4158
仟 <jhqian0228@icloud.com>
2016-12-27 23:11:20 +0800

    update interface

--- QPageViewExample/ViewController.m
+++ QPageViewExample/ViewController.m
@@ -24,12 +24,12 @@
     
     QPageView *pageView2 = [[QPageView alloc] initWithFrame:CGRectMake(0, 20, 300, 150)];
     
-    QPageView *pageView3 = [QPageView pageView];
+    QPageView *pageView3 = [QPageView q_pageView];
     
-    QPageView *pageView4 = [QPageView pageViewWithImageNames:@[@"img_00", @"img_01", @"img_02"]
-                                                  autoScroll:YES
-                                              autoScrollTime:2.0
-                                       pageIndicatorPosition:Right];
+    QPageView *pageView4 = [QPageView q_pageViewWithImageNames:@[@"img_00", @"img_01", @"img_02"]
+                                                    autoScroll:YES
+                                                autoScrollTime:2.0
+                                         pageIndicatorPosition:Right];
     
     NSLog(@"%@", pageView1);
     NSLog(@"%@", pageView2);
@@ -40,7 +40,7 @@
 - (void)pageViewDemo1 {
     
     // 创建分页视图控件
-    QPageView *pageView = [QPageView pageView];
+    QPageView *pageView = [QPageView q_pageView];
     
     pageView.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width / 2);
     
@@ -69,10 +69,10 @@
     NSArray *imageNameArr = @[@"img_00", @"img_01", @"img_02", @"img_03", @"img_04"];
     
     // 创建分页视图控件
-    QPageView *pageView = [QPageView pageViewWithImageNames:imageNameArr
-                                                 autoScroll:YES
-                                             autoScrollTime:3.0
-                                      pageIndicatorPosition:Center];
+    QPageView *pageView = [QPageView q_pageViewWithImageNames:imageNameArr
+                                                   autoScroll:YES
+                                               autoScrollTime:3.0
+                                        pageIndicatorPosition:Center];
     
     pageView.frame = CGRectMake(0,
                                 self.view.bounds.size.width / 2 + 20,
